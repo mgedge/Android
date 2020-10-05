93b06f53439e218b66d8daec9fe10be8a55b2c73
Matthew Gedge <matthew.gedge@gmail.com>
2020-10-05 13:41:41 -0500

    Assignment 6 started

---
+++ CS322-Intro_To_Android/Assignment6_Radiobuttons/app/src/main/java/z1818828/cs/niu/assignment6_radiobuttons/MainActivity.java
@@ -0,0 +1,14 @@
+package z1818828.cs.niu.assignment6_radiobuttons;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
