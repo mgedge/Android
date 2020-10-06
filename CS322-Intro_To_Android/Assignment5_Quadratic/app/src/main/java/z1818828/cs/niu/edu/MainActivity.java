99a8873e67adddfbb64e3de3cd32efb2bfe4926e
Matthew Gedge <matthew.gedge@gmail.com>
2020-10-05 23:37:24 -0500

    Improved code, started commentary

--- CS322-Intro_To_Android/Assignment5_Quadratic/app/src/main/java/z1818828/cs/niu/edu/MainActivity.java
+++ CS322-Intro_To_Android/Assignment5_Quadratic/app/src/main/java/z1818828/cs/niu/edu/MainActivity.java
@@ -1,15 +1,26 @@
+/***********************************************************************
+ CSCI 322/522  			  Assignment 5               		 FA2020
+
+ Class Name: MainActivity.java
+
+ Developer: Matthew Gedge
+ Due Date: 11 September 2020
+
+ Purpose: This java class starts the activity and runs the UI.
+ When the calculate button is pressed, the inputs are checked for
+ validity and
+ *********************************************************************/
 package z1818828.cs.niu.edu;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-import android.text.Html;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.text.DecimalFormat;
 
@@ -20,8 +31,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ((TextView)findViewById(R.id.textViewTitle2)).setText(Html.fromHtml("Ax<sup>2</sup>+ Bx + C = 0"));
-        ((TextView)findViewById(R.id.textViewCalculatedQuadratic)).setText(Html.fromHtml("Ax<sup>2</sup>+ Bx + C = 0"));
+        ((TextView) findViewById(R.id.textViewTitle2)).setText(R.string.default_equation);
+        ((TextView) findViewById(R.id.textViewCalculatedQuadratic)).setText(R.string.default_equation);
 
         final Button calculateButton = findViewById(R.id.buttonCalculate);
         final Button clearButton = findViewById(R.id.buttonClear);
@@ -39,12 +50,11 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 //Check if empty, check if zero
-                if(TextUtils.isEmpty(editTextQuadraticA.getText().toString()) ||
+                if (TextUtils.isEmpty(editTextQuadraticA.getText().toString()) ||
                         TextUtils.isEmpty(editTextQuadraticB.getText().toString()) ||
                         TextUtils.isEmpty(editTextQuadraticC.getText().toString())) {
                     Toast.makeText(MainActivity.this, "Please enter values", Toast.LENGTH_SHORT).show();
-                }
-                else {
+                } else {
                     String temp = editTextQuadraticA.getText().toString();
                     double quadraticA = Double.parseDouble(temp);
 
@@ -54,34 +64,31 @@ public class MainActivity extends AppCompatActivity {
                     temp = editTextQuadraticC.getText().toString();
                     double quadraticC = Double.parseDouble(temp);
 
-                    if(quadraticA <= 0) {
+                    if (quadraticA <= 0) {
                         Toast.makeText(MainActivity.this, "A must be greater than 0!", Toast.LENGTH_LONG).show();
                         editTextQuadraticA.setText("");
-                    }
-                    else {
+                    } else {
                         DecimalFormat df = new DecimalFormat("0.00");
 
-                        int discriminates = calculateDiscriminate(quadraticA,quadraticB,quadraticC);
+                        int discriminates = calculateDiscriminate(quadraticA, quadraticB, quadraticC);
 
                         String quadratic = editTextQuadraticA.getText().toString() + "x² + " + editTextQuadraticB.getText().toString() + "x + " + editTextQuadraticC.getText().toString() + " = 0";
                         calculatedQuadratic.setText(quadratic);
 
-                        if(discriminates > 0) {
-                            double firstValue = calculateQuadratic(quadraticA,quadraticB,quadraticC, 1);
-                            double secondValue = calculateQuadratic(quadraticA,quadraticB,quadraticC, 0);
+                        if (discriminates > 0) {
+                            double firstValue = calculateQuadratic(quadraticA, quadraticB, quadraticC, 1);
+                            double secondValue = calculateQuadratic(quadraticA, quadraticB, quadraticC, 0);
 
                             xValue1.setText(df.format(firstValue));
                             xValue2.setText(df.format(secondValue));
-                        }
-                        else if(discriminates == 0) {
-                            double firstValue = calculateQuadratic(quadraticA,quadraticB,quadraticC, 1);
+                        } else if (discriminates == 0) {
+                            double firstValue = calculateQuadratic(quadraticA, quadraticB, quadraticC, 1);
 
                             xValue1.setText(df.format(firstValue));
-                            xValue2.setText("No value");
-                        }
-                        else {
-                            xValue1.setText("Imaginary");
-                            xValue2.setText("Imaginary");
+                            xValue2.setText(R.string.no_vale);
+                        } else {
+                            xValue1.setText(R.string.imaginary);
+                            xValue2.setText(R.string.imaginary);
                         }
                     }
                 }
@@ -100,32 +107,29 @@ public class MainActivity extends AppCompatActivity {
                 //Clear output
                 xValue1.setText("X");
                 xValue2.setText("X");
-                calculatedQuadratic.setText(Html.fromHtml("Ax<sup>2</sup>+ Bx + C = 0"));
+                calculatedQuadratic.setText(R.string.default_equation);
             }
         });
     }
 
     private double calculateQuadratic(double a, double b, double c, int pos) {
-        double quadratic = 0;
+        double quadratic;
+        double sqrt = Math.sqrt(Math.pow(b, 2) - (4 * a * c));
 
-        if(pos == 1) {
-            quadratic = (-b + Math.sqrt(Math.pow(b,2) - (4 * a * c))) / (2 * a);
-        }
-        else {
-            quadratic = (-b - Math.sqrt(Math.pow(b,2) - (4 * a * c))) / (2 * a);
+        if (pos == 1) {
+            quadratic = (-b + sqrt) / (2 * a);
+        } else {
+            quadratic = (-b - sqrt) / (2 * a);
         }
 
         return quadratic;
     }
 
     private int calculateDiscriminate(double a, double b, double c) {
-        int discriminate = 0;
+        int discriminate;
 
-        try {
-            discriminate = (int) ((int)(b * b) - (4 * a * c));
-        } catch (Exception e) {
-            System.err.println("Calculating discriminate failed! Reason: " + e);
-            return 0;
-        }
-        return discriminate;    }
+        discriminate = (int) ((int) (b * b) - (4 * a * c));
+
+        return discriminate;
+    }
 }
