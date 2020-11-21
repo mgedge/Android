393956b5f39694af73c0d2bee0df7bb62fb1b937
Matthew Gedge <matthew.gedge@gmail.com>
2020-11-20 22:23:07 -0600

    Initialization of Assignment 8

---
+++ CS322-Intro_To_Android/Assignment8_SQLite/app/src/main/java/edu/csi/niu/z1818828/assignment8_sqlite/MainActivity.java
@@ -0,0 +1,14 @@
+package edu.csi.niu.z1818828.assignment8_sqlite;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
