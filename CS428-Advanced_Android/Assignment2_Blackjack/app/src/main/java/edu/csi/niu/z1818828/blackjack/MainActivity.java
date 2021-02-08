7659a83e50ef5ef9fc7e964c59cc6768c2e417cc
Matthew Gedge <matthew.gedge@gmail.com>
2021-02-08 12:30:31 -0600

    Initial commit of Blackjack

---
+++ CS428-Advanced_Android/Assignment2_Blackjack/app/src/main/java/edu/csi/niu/z1818828/blackjack/MainActivity.java
@@ -0,0 +1,14 @@
+package edu.csi.niu.z1818828.blackjack;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
