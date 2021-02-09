9d229d4a66c5f83d3be06a5db5c043ec1c348fb1
Matthew Gedge <matthew.gedge@gmail.com>
2021-02-09 00:51:35 -0600

    Implemented Hit button and score keeping

--- CS428-Advanced_Android/Assignment2_Blackjack/app/src/main/java/edu/csi/niu/z1818828/blackjack/MainActivity.java
+++ CS428-Advanced_Android/Assignment2_Blackjack/app/src/main/java/edu/csi/niu/z1818828/blackjack/MainActivity.java
@@ -1,11 +1,20 @@
 package edu.csi.niu.z1818828.blackjack;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
+import androidx.core.content.res.ResourcesCompat;
 
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
@@ -13,11 +22,25 @@ import java.util.Random;
 public class MainActivity extends AppCompatActivity {
     Handler mHandler;
     Runnable mUpdate;
+    Button buttonHit;
+    Button buttonStand;
+    Button buttonDeal;
+    TextView textViewDebug;
+    TextView textViewPlayerScore;
+    TextView textViewDealerScore;
+    ImageView imageViewGameStatus;
+    ConstraintLayout screen;
 
 
     Card[] cards;
+    int gameStatus = 0; //0 - not active, 1 - active game, 2 - win/lose scenario
     int counter = 0;
+    int deckStack = 0;
+    int playerScore;
+    int dealerScore;
 
+    Deque<Card> playerStack = new ArrayDeque<Card>();
+    Deque<Card> dealerStack = new ArrayDeque<Card>();
     Map<String, Bitmap> bitmapCards = new HashMap<>();
 
     @Override
@@ -25,17 +48,121 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        buttonHit = findViewById(R.id.buttonHit);
+        buttonStand = findViewById(R.id.buttonStand);
+        buttonDeal = findViewById(R.id.buttonDeal);
+        textViewDebug = findViewById(R.id.textViewDebugCard);
+        textViewPlayerScore = findViewById(R.id.textViewPlayerScore);
+        textViewDealerScore = findViewById(R.id.textViewDealerScore);
+        imageViewGameStatus = findViewById(R.id.imageViewGameStatus);
+        screen = findViewById(R.id.gameScreen);
+
+        switchGameStatusView();
+
         cards = new Card[52];
         cards = initialize(cards);
         cards = shuffleDeck(cards);
 
+        //mHandler = new Handler();
+        //mHandler.post(mUpdate);
 
-        mHandler = new Handler();
-        mHandler.post(mUpdate);
+        screen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(gameStatus == 2) {
+                    resetGame();
+                }
+            }
+        });
 
+        buttonDeal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Start the game status
+                gameStatus = 1;
+                switchGameStatusView();
+
+                //Deal two cards to the player
+                playerStack.add(cards[deckStack]);
+                playerScore += evaluateCardScore(cards[deckStack].rank);
+                deckStack++;
+                playerStack.add(cards[deckStack]);
+                playerScore += evaluateCardScore(cards[deckStack].rank);
+                deckStack++;
+
+                //Deal two cards to the dealer
+                dealerStack.add(cards[deckStack]);
+                dealerScore += evaluateCardScore(cards[deckStack].rank);
+                deckStack++;
+                dealerStack.add(cards[deckStack]);
+                dealerScore += evaluateCardScore(cards[deckStack].rank);
+                deckStack++;
+                updateGameScore();
+            }
+        });
+
+        buttonHit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(gameStatus != 2) {
+                    textViewDebug.setText("Card num: " + deckStack + " Suit: " + cards[deckStack].suit + " Rank: " + cards[deckStack].rank + " Card: " + cards[deckStack].cardPicture);
+                    playerStack.add(cards[deckStack]);
+                    playerScore += evaluateCardScore(cards[deckStack].rank);
+                    deckStack++;
+                    updateGameScore();
+
+                    if (playerScore > 21) {
+                        gameStatus = 2;
+                        imageViewGameStatus.setImageDrawable(ResourcesCompat.getDrawable(getResources(), R.drawable.lose, null));
+                        imageViewGameStatus.setVisibility(View.VISIBLE);
+                        cards = null;
+                    }
+                    else if (playerScore == 21) {
+                        gameStatus = 2;
+                        imageViewGameStatus.setImageDrawable(ResourcesCompat.getDrawable(getResources(), R.drawable.win, null));
+                        imageViewGameStatus.setVisibility(View.VISIBLE);
+                        cards = null;
+                    }
+                }
+                else {
+                    resetGame();
+                }
+            }
+        });
+
+        buttonStand.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(gameStatus != 2) {
+
+                }
+                else {
+                    resetGame();
+                }
+            }
+        });
+    }
+
+    public void switchGameStatusView() {
+        if((gameStatus == 1) || (gameStatus == 2)){
+            buttonHit.setVisibility(View.VISIBLE);
+            buttonStand.setVisibility(View.VISIBLE);
+            buttonDeal.setVisibility(View.INVISIBLE);
+            textViewPlayerScore.setVisibility(View.VISIBLE);
+            textViewDealerScore.setVisibility(View.VISIBLE);
+        }
+        else {
+            buttonHit.setVisibility(View.INVISIBLE);
+            buttonStand.setVisibility(View.INVISIBLE);
+            buttonDeal.setVisibility(View.VISIBLE);
+            textViewPlayerScore.setVisibility(View.INVISIBLE);
+            textViewDealerScore.setVisibility(View.INVISIBLE);
+        }
     }
 
     public Card[] initialize(Card[] deck) {
+        counter = 0;
+
         //Initialize the deck
         for(int suit = 0; suit < 4; suit++) {
             for(int rank = 0; rank < 13; rank++) {
@@ -101,5 +228,45 @@ public class MainActivity extends AppCompatActivity {
 
         return deck;
     }
-}
 
+    public void updateGameScore() {
+        try {
+            textViewDealerScore.setText(String.valueOf(dealerScore));
+            textViewPlayerScore.setText(String.valueOf(playerScore));
+        }
+        catch (Exception e) {
+            System.out.println("Failed to update game score - " + e);
+        }
+    }
+
+    public int evaluateCardScore(int rank) {
+        if(rank == 0) {
+            if(playerScore > 10)
+                return 1;
+            else
+                return 11;
+        }
+        else if(rank > 9)
+            return 10;
+        else
+            return rank + 1;
+    }
+
+    public void resetGame() {
+        dealerScore = 0;
+        playerScore = 0;
+
+        dealerStack.clear();
+        playerStack.clear();
+
+        gameStatus = 0;
+        deckStack = 0;
+        switchGameStatusView();
+        imageViewGameStatus.setVisibility(View.INVISIBLE);
+
+        cards = new Card[52];
+        cards = initialize(cards);
+        cards = shuffleDeck(cards);
+
+    }
+}
---
+++ CS428-Advanced_Android/Assignment2_Blackjack/app/src/main/java/edu/csi/niu/z1818828/blackjack/Panel.java
@@ -0,0 +1,31 @@
+package edu.csi.niu.z1818828.blackjack;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import androidx.annotation.NonNull;
+
+public class Panel extends SurfaceView implements SurfaceHolder.Callback {
+    public Panel(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        getHolder().addCallback(this);
+    }
+
+    @Override
+    public void surfaceCreated(@NonNull SurfaceHolder holder) {
+
+    }
+
+    @Override
+    public void surfaceChanged(@NonNull SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(@NonNull SurfaceHolder holder) {
+
+    }
+}
